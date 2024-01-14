29ebab8e3fa1653cdb56da9291874eecd0419e55
uc4151 <gemmalinda19@gmail.com>
2024-01-14 10:23:17 -0800

    Task 2 REDO Again

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
+
+
 if __name__ == "__main__":
+
     """Print the number of and list of arguments."""
-    from sys import argv
 
-    count = len(argv) - 1
+    import sys
+
+
+    count = len(sys.argv) - 1
+
     if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
 
+        print("0 arguments.")
+
+    elif count == 1:
+
+        print("1 argument:")
+
+    else:
+
+        print("{} arguments:".format(count))
+
+    for i in range(count):
+
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))

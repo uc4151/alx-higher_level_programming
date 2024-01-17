7bacd2dc4e570337c775735bbda5b3ad9fc81914
uc4151 <gemmalinda19@gmail.com>
2024-01-17 04:21:35 -0800

    second commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
         return my_list
     elif idx >= len(my_list):
         return my_list
-     new_list = list(my_list)
+    new_list = list(my_list)
     new_list[idx] = element
     return new_list

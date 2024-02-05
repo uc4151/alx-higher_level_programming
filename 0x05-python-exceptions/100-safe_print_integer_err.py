70064c18621eb47235541a9e64511e066635924f
uc4151 <gemmalinda19@gmail.com>
2024-02-04 20:31:56 -0800

    task 100 Second redo

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value), end"")
+        print("{:d}".format(value), end="")
         return True
     except (ValueError, TypeError) as err:
         print("Exception: {}".format(err), file =sys.stderr)

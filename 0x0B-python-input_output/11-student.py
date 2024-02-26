558dd9f5705e983e32e1081a095ede1b7f083965
uc4151 <gemmalinda19@gmail.com>
2024-02-25 21:38:58 -0800

    second commit

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -26,7 +26,7 @@ class Student:
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
-        if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
+        if (type(attrs) is list and all(type(ele) is str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 

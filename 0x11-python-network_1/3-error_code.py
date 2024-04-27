ec9b8be488734953914371c0f2995c265797f8e6
uc4151 <gemmalinda19@gmail.com>
2024-04-26 20:42:36 -0700

    Task 3 commit

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 """
-Script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8
+Script that takes in a URL, send a request to URL, and dispaly body
 """
-import sys
-import urllib.error
-import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    import sys
+    from urllib import request, error
 
-    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        with request.urlopen(sys.argv[1]) as resp:
+            print(resp.read().decode('UTF-8'))
+    except error.HTTPError as er:
+        print('Error code:', er.code)

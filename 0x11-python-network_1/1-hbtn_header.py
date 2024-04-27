380828be5dd5c9febc4b11ef437019b2c50ee871
uc4151 <gemmalinda19@gmail.com>
2024-04-26 19:25:07 -0700

    Task 1 commit

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,9 +7,9 @@ from sys import argv
 from urllib.request import Request, urlopen
 
 
-if __name__ == "__main__":
     url = argv[1]
     req = Request(url)
 
     with urlopen(req) as response:
         print(dict(response.headers).get("X-Request-Id"))
+if __name__ == "__main__":

534bf1a4c8af1dbe3f6bcc41e9b7ae6a82dfd705
uc4151 <gemmalinda19@gmail.com>
2024-04-26 20:26:46 -0700

    Task 10 commit

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    reqst = requests.get(url,
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(reqst.json().get('id'))
